62dfee152f83501c01d9d57939984bc42102816e
Anthony15224 <Anthony15224@hotmail.com>
2019-11-22 20:48:14 -0600

    SQL update to's

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
-var mySQL=require("mysql");
-var inquirer=require("inquirer");
 
+var inquirer=require("inquirer");
 var mysql = require("mysql");
+var answerObject="";
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -19,64 +19,71 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+//   console.log("connected as id " + connection.threadId);
+    showCase()
 });
 
+function showCase(){
+    connection.query("SELECT * from bamazon_db.products" , function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+          console.log("Item ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Department: " + res[i].department_name +
+           " || Price: " + res[i].price + " || Quantity: " + res[i].stock_quantity + "\n");
+        }
+    customerPrompt(res);
+});
+}
+
+function customerPrompt(res){
 inquirer.prompt([
-
-    {
-      type: "text",
-      name: "userInput",
-      message: "What is the ID of the product you would like to purchase?",
-      
-    }
-  
-  // After the prompt, store the user's response in a variable called location.
-  ]).then(function(answer) {
-    if(answer.userInput=="POST"){
-      post();}    
-    else{
-      bid();
-    }
-});
-  
-function bid(){
-  inquirer.prompt([
-
     {
       type: "input",
-      name: "userInput",
-      message: "What is your bid?", 
+      name: "inputID",
+      message: "What is the ID of the product you would like to purchase?",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+      type: "input",
+      name: "inputQuantity",
+      message: "How many units would you like to purchase?"
     }
   
   // After the prompt, store the user's response in a variable called location.
   ]).then(function(answer) {
-    var bid=answer.userInput;
-    connection.query("SELECT HIGH_BID FROM greatBay_db.auction", function(err, res) {
-      if (err) throw err; 
-      for(let i=0; i<res.length; i++){
-        if(res[i].HIGH_BID>bid){
-          updateHigh(bid)
-        }
-        else{
-          updateLow()
-        }
+      answerObject={
+      item_Id: answer.inputID,
+      item_Quantity: answer.inputQuantity
+    }
+    for (var i = 0; i < res.length; i++) {
+    if(answerObject.item_Id==res[i].item_id && answerObject.item_Quantity<=res[i].stock_quantity) {
+      var inventoryAdjustment=res[i].stock_quantity-answerObject.item_Quantity;
+      // var total=res[i].price*answerObject.item_Quantity;
+      connection.query("UPDATE bamazon_db.products SET? WHERE?",
+      [{stock_quantity:inventoryAdjustment},{item_id:res[i].item_id}])
+        
+      //  console.log(total) 
+        // function(err, res) {
+        //   if (err) throw err;
+           
       }
-      // Log all results of the SELECT statement
-     
-    });
-});
+      var total=res[i].price*answerObject.item_Quantity;
+      console.log(total) 
 
+    // 
+    }
+    
+   showCase(); 
+  });
+  // console.log("Invalid Product ID");
+  
 }
 
-function updateHigh(bid){
-  connection.query("INSERT INTO auction(HIGH_BID) VALUES (res[i].HIGH_BID)", function(err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-connection.end();
-}
-};
+
 
 
   
