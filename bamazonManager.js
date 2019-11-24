e1dc86de62a8eb1e5ebd665c1e1559ba7d652651
Anthony15224 <Anthony15224@hotmail.com>
2019-11-23 21:12:16 -0600

    add, view, view low--manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -30,23 +30,17 @@ inquirer.prompt([
   
   // After the prompt, store the user's response in a variable called location.
   ]).then(function(answer) {
-      console.log()
    switch(answer.manageInventory){
     case "View Products for Sale":
-        console.log("YES")
-        // viewInventory();
+        viewInventory();
         break;
 
     case "View Low Inventory":
-            console.log("YES")
-
-        // viewLowInventory();
+        viewLowInventory();
         break;
     
     case "Add to Inventory":
-        // addInventory();
-        console.log("YES")
-
+        addInventory();
         break;
 
     case "Add New Product":
@@ -58,6 +52,63 @@ inquirer.prompt([
    }
   });
 }
+
+function viewInventory(){
+    connection.query("SELECT * from bamazon_db.products" , function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+          console.log("Item ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Department: " + res[i].department_name +
+           " || Price: " + res[i].price + " || Quantity: " + res[i].stock_quantity + "\n");
+        }
+        managePrompt()
+   });
+}
+
+function viewLowInventory(){
+    // var lowInventory= stock_quantity<10;
+    connection.query("SELECT * from bamazon_db.products WHERE stock_quantity<10", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+          console.log("Item ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Department: " + res[i].department_name +
+           " || Price: " + res[i].price + " || Quantity: " + res[i].stock_quantity + "\n");
+        }
+        managePrompt()
+   });
+}
+
+function addInventory(){
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "inputID",
+          message: "What is the ID of the product you would like to manage?",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+              return false;
+          }
+        }
+    ]).then(function(answer) {
+        // connection.query("SELECT * from bamazon_db.products" , function(err, res) {
+        // for(let i=0; i<res.length; i++){
+
+        // }
+        // });
+        // var answerObject={
+        //     itemID:answer.inputID
+        // }
+        console.log(answerObject)
+        connection.query("SELECT * from bamazon_db.products WHERE item_id=" + answer.inputID, function(err, res) {
+            for(let i=0; i<res.length; i++){
+                console.log(res[i])
+            }
+    // //    connection.query("INSERT INTO bamazon_db.products.stock_quantity WHERE stock_quantity<10", function(err, res) {
+
+    });
+});
+}
+
 
 
 
